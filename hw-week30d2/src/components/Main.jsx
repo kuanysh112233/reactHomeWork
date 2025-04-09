5a528fee74abdfc59388d76ea5426ad170aa11a6
Kuanysh <tkuanys767@gmail.com>
2025-04-09 08:45:06 +0500

    useContext

--- hw-week30d2/src/components/Main.jsx
+++ hw-week30d2/src/components/Main.jsx
@@ -1,19 +1,18 @@
-import React, { useContext } from 'react'
-import {LanguageContext} from "./LanguageProvider"
+import React, { useContext } from 'react';
+import { LanguageContext } from "./LanguageProvider";
 
 export default function Main() {
-    
-    const{translations, langIndex, changeLanguage} = useContext(LanguageContext)
+  const { translations, langIndex, changeLanguage } = useContext(LanguageContext);
 
   return (
     <div>
-      <h2>{translations[0]}</h2>
-      <p>{translations[1]}</p>
-      <select value={langIndex} onChange={(e)=> changeLanguage(Number(e.target.value))}>
-        <option value={0}>ҚАЗАКША</option>
-        <option value={1}>РУСКИЙ</option>
+      <h2>{translations?.[0] || ""}</h2>
+      <p>{translations?.[1] || ""}</p>
+      <select value={langIndex} onChange={(e) => changeLanguage(Number(e.target.value))}>
+        <option value={0}>ҚАЗАҚША</option>
+        <option value={1}>РУССКИЙ</option>
         <option value={2}>ENGLISH</option>
       </select>
     </div>
-  )
+  );
 }
